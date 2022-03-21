a254ada65c3f1480b4d9a2a6aaf92a0e9cde534f
Henrique Naimaier <coubron@yahoo.com.br>
2022-03-21 20:05:23 -0300

    Added delete and visit functions

--- page-tracker.js
+++ page-tracker.js
@@ -2,23 +2,22 @@ window.onload = function() {
     showStoredData();
 }
 
-
 sites = [
     {
         id: 1,
         title: "Google",
         url: "http://www.google.com",
-        lastVisit: new Date()
+        lastVisit: new Date("05/05/2005")
     }, {
         id: 2,
         title: "G1",
         url: "http://www.g1.com.br",
-        lastVisit: new Date()
+        lastVisit: new Date("10/10/2010")
     }, {
         id: 3,
         title: "DW",
         url: "http://www.dw.com",
-        lastVisit: new Date()
+        lastVisit: new Date("12/12/2012")
     }];
 
 
@@ -40,11 +39,35 @@ function createListElement(site) {
     
     li.innerHTML = `<a href=${site.url} target=blank>${site.title}</a> | ${site.lastVisit.toLocaleDateString("pt-br")}`;
 
+    // TODO metodo visitar: se nao tiver hj na pilha, adiciona
+    let todayButton = document.createElement("button");
+    todayButton.innerText = "Today";
+    todayButton.onclick = function() {
+        site.lastVisit = new Date();
+        
+        showStoredData();
+    }
+    li.appendChild(todayButton);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = function() {
+        deleteSite(site.id);
+
+        showStoredData();
+    }
+    li.appendChild(deleteButton);
+
     return li;
 }
 
 function addSite() {}
 
-function deleteSite() {}
+function deleteSite(id) {
+    
+    for (let i = 0; i < sites.length; i++) {
+        if (sites[i].id != id) continue;
 
-function updateSite() {}
+        sites.splice(i, 1);
+    }
+}
